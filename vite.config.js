733a67655a5b35929499dfc040cb80b3fac92c95
root_obaika <57393895+rootobaika@users.noreply.github.com>
2026-04-11 10:44:56 +0500

    add support android

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  assetsInclude: ['**/*.apk'],
   clearScreen: false,
   envPrefix: ['VITE_', 'TAURI_'],
   server: {
